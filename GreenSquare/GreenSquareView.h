d728d2883656c5496a9b85b0e4bd400c48670a86
Rob Haining <rob@tolar.ws>
2020-04-09 14:57:29 -0400

    Initial Commit

---
+++ GreenSquare/GreenSquareView.h
@@ -0,0 +1,13 @@
+//
+//  GreenSquareView.h
+//  GreenSquare
+//
+//  Created by Robert Tolar Haining on 4/9/20.
+//  Copyright © 2020 Robert Tolar Haining. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface GreenSquareView : ScreenSaverView
+
+@end
